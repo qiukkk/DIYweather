2ef1067bebdb91e5b09219c3d155747658e97a2f
qiukkk <1114362430@qq.com>
2017-04-04 20:50:56 +0800

    First commit

---
+++ app/src/main/java/d/l/e/i/h/s/com/diyweather/MainActivity.java
@@ -0,0 +1,13 @@
+package d.l.e.i.h.s.com.diyweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
